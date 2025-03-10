66c51c3d15e335a321957299ac882e28a85af612
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-03-10 22:28:11 +0300

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import Login from "./components/Login";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-orange  text-2xl">
-        Wel-Cone To Harar Election Board
-      </h1>
+     <h1 className="text-white text-5xl text-center">WEL-COME TO ELECTION BOARD OF HARAR </h1>
+      <Login />
     </div>
   );
 };
