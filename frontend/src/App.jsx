824ded4313126e7e712dbf00f053745af349439e
belo-7 <blenterefe611@gmail.com>
2025-03-10 21:14:24 +0300

    configure the folder by clone

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-orange  text-2xl">WEL-COME TO HARAR VOTING </h1>
+    </div>
+  );
+};
+
+export default App;
